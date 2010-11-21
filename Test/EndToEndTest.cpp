6a013f0986410fbfa071779cdf88e46ab979b659
Ilya <Ilya@localhost>
2010-11-21 10:47:37 +0100

    Fix Linux build

--- Test/EndToEndTest.cpp
+++ Test/EndToEndTest.cpp
@@ -18,6 +18,7 @@
     along with this program.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+#include <cstdio>
 #include "Test.h"
 #include "Cornucopia.h" //includes everything necessary to use the library
 
